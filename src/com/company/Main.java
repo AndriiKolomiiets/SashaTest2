22106b1173f02819b789ce0d16ce7a68f09a139d
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-07-05 23:47:52 +0300

    Simple variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,21 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        short S = 3;
+        int I = 235;
+        long L = 13548;
+        double D = 12.5;
+        float F = 12.3F;
+        boolean B = true;
+        System.out.print("The first variable is " + S);
+        System.out.print(", the second variable is " + I);
+        System.out.print(", the third variable is " + L);
+        System.out.print(", the fourth variable is " + D);
+        System.out.print(", the fifth variable is " + F);
+        System.out.print(", the sixth variable is " + B);
 
     }
 }
