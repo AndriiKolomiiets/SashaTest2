795ee24dde7f70e26f8830c91cf248551aec66e4
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-07-15 21:56:06 +0300

    TheGame stage 1

---
+++ src/com/company/TheGame.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.*;
+
+public class TheGame {
+
+// It is where the Game begins
+
+
+    final int random;
+
+    public TheGame() {
+        random = (int) (Math.random() * 100 + 1);
+        System.out.println("The computer's number is " + random);
+        System.out.println("*the line above was displayed just for checking mistakes");
+        System.out.println(" ");
+
+
+    public void PerformTheGame() {
+        int theNumber = 0;
+        int i = 0;
+        Scanner userInput = new Scanner(System.in);
+        while (theNumber != 500) {
+            i++;
+            // Getting User input
+            System.out.println("It is your " + i + " attempt");
+
+            System.out.println("Enter your number ");
+            theNumber = userInput.nextInt();
+
+
+            // Checking for the winner
+            if (random == theNumber) {
+                System.out.println("You win! Computer number was " + random);
+                theNumber = 500;
+            } else if (random < theNumber) {
+                System.out.println("It is less. Try again");
+            } else {
+                System.out.println("It is more. Try again");
+            }
+        }
+        userInput.close();
+    }
+
+
+}
+
+
+}
