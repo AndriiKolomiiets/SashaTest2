207e4cd6aab78c4c481ef3a668944d74465e0c6f
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-07-15 22:43:59 +0300

    Person input

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,5 +1,19 @@
 package com.company;
 
+import java.util.*;
+
 public class Person {
+
+    Scanner userInput = new Scanner(System.in);
+
+    public void getUserInput() {
+        System.out.println("Enter your name:");
+        String name = userInput.next();
+        System.out.println("Enter your surname:");
+        String surname = userInput.next();
+        System.out.println("How old are you?");
+        String age = userInput.next();
+        userInput.close();
+    }
 
 }
