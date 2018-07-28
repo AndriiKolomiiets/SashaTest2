6d8088af2ef8dbe70bebdb29dbfb3fce17d75932
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-07-28 23:28:47 +0300

    Writing contacts without outputting. //reduced functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("The contact input program\nWould you like to create a new contact? (Yes/No)");
-        Scanner inputAnsver = new Scanner(System.in);
-        String theAnsver = inputAnsver.nextLine();
+        Scanner inputAnswer = new Scanner(System.in);
+        String theAnsver = inputAnswer.nextLine();
         Person[] unit = new Person[12];
         int i = 0;
         while (theAnsver.equals("Yes")) {
@@ -17,24 +17,25 @@ public class Main {
             unit[i].getUserInput();
             i++;
             System.out.println("Do you wont to create one more new contact? (Yes/No)");
-            theAnsver = inputAnsver.nextLine();
+            theAnsver = inputAnswer.nextLine();
         }
         int n = i++;
         System.out.println("You saved " + n + " contacts.");
+        String byeMessage = "Thank you for using this app!\nHave a nice day!";
         if (n > 0) {
             System.out.println("Would you like to print them out to check?");
         } else {
-            System.out.println("Thank you for using this app!\nHave a nice day!");
+            System.out.println(byeMessage);
         }
-      /*  String printOutInput = inputAnsver.nextLine();
+      /*  String printOutInput = inputAnswer.nextLine();
 
         if (printOutInput.equals("Yes")) {
             for (int k = i + 1; i > 0; i--) {
                 System.out.println(unit[i]);
             }
-            System.out.println("Thank you for using this app!\nHave a nice day!");
+            System.out.println("byeMessage");
         } else {
-            System.out.println("Thank you for using this app!\nHave a nice day!");
+            System.out.println("byeMessage");
         }*/
     }
 }
