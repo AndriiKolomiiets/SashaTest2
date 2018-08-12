246148a8c2aa5caebc97f28dd3a0170ab80a9745
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-08-13 00:04:17 +0300

    BubbleSorting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,24 @@
 package com.company;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        int sort[] = {12, 65, 687, 14, 10, 56, 18, 954, 1478, 1, 9, 6};
 
+        for (int i = sort.length-1; i>=0; i--) {
+            for (int j = 0; j < i; j++)
+                if (sort[j] > sort[j + 1]) {
+                    int temp = sort[j];
+                    sort[j] = sort[j + 1];
+                    sort[j + 1] = temp;
+                }
+        }
+        for (int i = 0; i < sort.length; i++) {
+            System.out.print(sort[i] + ", ");
+        }
+//        System.out.println(Array%.toString);
     }
-
-
-        
-
-    }
+}
