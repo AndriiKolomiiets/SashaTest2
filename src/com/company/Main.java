ebd8031c516c20cfb710bb1723092c1709b1e6cc
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-08-12 16:43:27 +0300

    scratch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,14 @@
 package com.company;
+
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int fibonacci = 0;
-        int nextFibonacci = 1;
-        int fibonacciSeries = 1;
-        System.out.println("Please input the number up to fibonacci series you would like to print out:");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        //Iteration block
-        for (int i = 0; i < n; i++) {
-            System.out.print(fibonacciSeries + " ");
-            fibonacciSeries = fibonacci + nextFibonacci;
-            fibonacci = nextFibonacci;
-            nextFibonacci = fibonacciSeries;
+    }
 
-        }
 
         
 
     }
-}
