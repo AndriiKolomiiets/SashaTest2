3605f4e546172efdb441cdbc7cb9b7a14164b578
Andrii Kolomiiets <andrew.kolomiyets@gmail.com>
2018-08-15 22:56:39 +0300

    BubbleSorting SelectSorting

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,23 +5,51 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        int sort[] = {687, 12, 6};
-        System.out.println(Arrays.toString(sort));
-        int minIndex = 0;
 
-        for (int i = 0; i < sort.length-1; i++) {
-            for (int j = i; j<sort.length; j++) {
-                if (sort[minIndex] > sort[j]) {
-                    minIndex = j;
+    public static void main(String[] args) {
+        int testArray[] = {687, 12, 6, 4, 1, 23, 345, 0, 9, 9, 45896};
+        System.out.println(Arrays.toString(testArray));
+
+//        bubbleSort(testArray);
+        selectSort(testArray);
+
+    }
+
+    //Sort by bubble method
+    public static void bubbleSort(int arrayToSort[]) {
+        System.out.println("1");
+        for (int i = arrayToSort.length - 1; i < 0; i--) {
+            System.out.println("2");
+            for (int j = 0; j > i; j++) {
+                if (arrayToSort[j] > arrayToSort[j + 1]) {
+                    int tmp = arrayToSort[j];
+                    arrayToSort[j] = arrayToSort[j + 1];
+                    arrayToSort[j + 1] = tmp;
+                    System.out.println(arrayToSort[j] + arrayToSort[j + 1]);
                 }
             }
-            int temp = sort[i];
-            sort[i] = sort[minIndex];
-            sort[minIndex] = temp;
-        }
-        System.out.println(sort[minIndex]);
 
-        System.out.println(Arrays.toString(sort));
+            System.out.println(Arrays.toString(arrayToSort));
+
+        }
+
+
+    }
+
+    public static void selectSort(int arrayToSort2[]) {
+
+        int minIndex = arrayToSort2[0];
+        for (int j = 0; j < arrayToSort2.length; j++) {
+            for (int i = j; i < arrayToSort2.length; i++) {
+
+                if (arrayToSort2[i] < minIndex) {
+                    minIndex = arrayToSort2[i];
+                }
+
+            }
+            arrayToSort2[j] = minIndex;
+
+        }
+        System.out.println(Arrays.toString(arrayToSort2));
     }
 }
